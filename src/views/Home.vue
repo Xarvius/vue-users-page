7efba61d30babf705a1b08408bc8ea9000bd3a99
Xarvius <strzelecki@gmail.com>
2020-10-05 15:25:51 +0200

    Remove users list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <ProfileList  v-for="profile in profiles" :key="profile.user.id" :profile=profile />
   </div>
 </template>
 
 <script>
 // import router from './router/index'
-import ProfileList from '../components/ProfilesList.vue'
 import gql from 'graphql-tag'
 const GET_PROFILES = gql`query getProfiles{
       profiles{
@@ -23,9 +21,6 @@ export default {
     profiles: {
       query: GET_PROFILES
     }
-  },
-  components: {
-    ProfileList
   }
 }
 </script>
