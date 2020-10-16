dbac503bc3ad19c7d81ff6914b60a08033b55fc8
Xarvius <strzelecki@gmail.com>
2020-10-16 14:41:41 +0200

    add card of user

---
+++ src/components/UsersList.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-card
+        :title="fullName" :sub-title="profile.email"
+    >
+    </b-card>
+</template>
+
+<script>
+export default {
+    props: {
+        profile: {}
+    },
+    computed:{
+        fullName: function(){
+            return `${this.profile.firstName} ${this.profile.lastName}`
+        }
+    }
+}
+</script>
+
+<style scoped>
+.card {
+    margin: 1% 5%;
+}
+</style>
